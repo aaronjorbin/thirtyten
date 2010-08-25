ecaf9f23817f5f3a0a98491fd96046bb5bcd660a
Aaron Jorbin <aaron.jorbin@gmail.com>
2010-08-25 19:36:26 +0000

    Update cat/tag functions in loop-author.php.  Fixes #1

--- loop-author.php
+++ loop-author.php
@@ -138,13 +138,23 @@
 	<?php endif; ?>
 
 			<div class="entry-utility">
-				<span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php echo twentyten_cat_list(); ?></span></span>
+				<?php if ( count( get_the_category() ) ) : ?>
+				 <span class="cat-links">
+				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+				</span>	
 				<span class="meta-sep"> | </span>
-				<?php $tags_text = twentyten_tag_list(); ?>
-				<?php if ( ! empty( $tags_text ) ) : ?>
-				<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links"><?php echo $tags_text; ?></span></span>
-				<span class="meta-sep"> | </span>
-				<?php endif; //$tags_text ?>
+				<?php endif; ?>
+
+				<?php
+				$tags_list = get_the_tag_list( '', ', ' );
+				if ( $tags_list ):
+				?>
+				<span class="tag-links">
+	                                         <?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+	                                        </span>
+	                                        <span class="meta-sep">|</span>
+	                                <?php endif; ?>
+
 				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
 				<?php edit_post_link( __( 'Edit', 'twentyten' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t\n" ); ?>
 			</div><!-- #entry-utility -->
